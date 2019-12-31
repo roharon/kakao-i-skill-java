f3b1c2f8ce80c5e0006ed565379e1d27df9b767b
Aaron Roh <roharon98@gmail.com>
2019-12-31 16:33:57 +0900

    add: type Profile

--- type/Profile.java
+++ type/Profile.java
@@ -1,4 +1,14 @@
 package com.roharon.huformationi.wrapper.type;
 
+import lombok.*;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@ToString
 public class Profile {
+    private String nickname;
+    private String imageUrl;
 }
