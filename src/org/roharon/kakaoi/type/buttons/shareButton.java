be4ac8d8d53041e7efa6bc83ee5bc07bd998c40b
Aaron Roh <roharon98@gmail.com>
2020-01-01 17:32:46 +0900

    add: shareButton type

---
+++ src/org/roharon/kakaoi/type/buttons/shareButton.java
@@ -0,0 +1,16 @@
+package org.roharon.kakaoi.type.buttons;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.ToString;
+import org.roharon.kakaoi.type.Button;
+
+@Getter
+@Builder
+@ToString
+public class shareButton implements Button {
+
+    private String label;
+    @Builder.Default
+    private String action = "phone";
+}
